6065005183eba4871956fc8515227cd7970eff9e
PeytonCunningham <pecu1461@colorado.edu>
2025-05-11 00:14:07 -0600

    Update tests.c

--- tests.c
+++ tests.c
@@ -25,7 +25,7 @@ unsigned f2u(float f) {
   return a.u;
 }
 
-/* Copyright (C) 1991-2018 Free Software Foundation, Inc.
+/* Copyright (C) 1991-2022 Free Software Foundation, Inc.
    This file is part of the GNU C Library.
 
    The GNU C Library is free software; you can redistribute it and/or
@@ -40,7 +40,7 @@ unsigned f2u(float f) {
 
    You should have received a copy of the GNU Lesser General Public
    License along with the GNU C Library; if not, see
-   <http://www.gnu.org/licenses/>.  */
+   <https://www.gnu.org/licenses/>.  */
 /* This header is separate from features.h so that the compiler can
    include it implicitly at the start of every compilation.  It must
    not itself include <features.h> or any other header that includes
@@ -60,27 +60,15 @@ unsigned f2u(float f) {
    - 56 emoji characters
    - 285 hentaigana
    - 3 additional Zanabazar Square characters */
-/* We do not support C11 <threads.h>.  */
 int test_bitOr(int x, int y)
 {
   return x|y;
 }
-int test_evenBits(void) {
-  int result = 0;
-  int i;
-  for (i = 0; i < 32; i+=2)
-    result |= 1<<i;
-  return result;
+int test_isZero(int x) {
+  return x == 0;
 }
-int test_minusOne(void) {
-  return -1;
-}
-int test_allEvenBits(int x) {
-  int i;
-  for (i = 0; i < 32; i+=2)
-      if ((x & (1<<i)) == 0)
-   return 0;
-  return 1;
+int test_tmax(void) {
+  return 0x7FFFFFFF;
 }
 int test_anyOddBit(int x) {
     int i;
@@ -89,127 +77,68 @@ int test_anyOddBit(int x) {
       return 1;
     return 0;
 }
-int test_byteSwap(int x, int n, int m)
+int test_fitsBits(int x, int n)
 {
-    /* little endiamachine */
-    /* least significant byte stored first */
-    unsigned int nmask, mmask;
-    switch(n) {
-    case 0:
-      nmask = x & 0xFF;
-      x &= 0xFFFFFF00;
-      break;
-    case 1:
-      nmask = (x & 0xFF00) >> 8;
-      x &= 0xFFFF00FF;
-      break;
-    case 2:
-      nmask = (x & 0xFF0000) >> 16;
-      x &= 0xFF00FFFF;
-      break;
-    default:
-      nmask = ((unsigned int)(x & 0xFF000000)) >> 24;
-      x &= 0x00FFFFFF;
-      break;
-    }
-    switch(m) {
-    case 0:
-      mmask = x & 0xFF;
-      x &= 0xFFFFFF00;
-      break;
-    case 1:
-      mmask = (x & 0xFF00) >> 8;
-      x &= 0xFFFF00FF;
-      break;
-    case 2:
-      mmask = (x & 0xFF0000) >> 16;
-      x &= 0xFF00FFFF;
-      break;
-    default:
-      mmask = ((unsigned int)(x & 0xFF000000)) >> 24;
-      x &= 0x00FFFFFF;
-      break;
-    }
-    nmask <<= 8*m;
-    mmask <<= 8*n;
-    return x | nmask | mmask;
+  int TMin_n = -(1 << (n-1));
+  int TMax_n = (1 << (n-1)) - 1;
+  return x >= TMin_n && x <= TMax_n;
 }
-int test_addOK(int x, int y)
-{
-    long long lsum = (long long) x + y;
-    return lsum == (int) lsum;
-}
-int test_conditional(int x, int y, int z)
-{
-  return x?y:z;
+int test_leastBitPos(int x) {
+  int mask = 1;
+  if (x == 0)
+    return 0;
+  while (!(mask & x)) {
+    mask = mask << 1;
+  }
+  return mask;
 }
 int test_isAsciiDigit(int x) {
   return (0x30 <= x) && (x <= 0x39);
 }
-int test_replaceByte(int x, int n, int c)
+int test_isLessOrEqual(int x, int y)
 {
-    switch(n) {
-    case 0:
-      x = (x & 0xFFFFFF00) | c;
-      break;
-    case 1:
-      x = (x & 0xFFFF00FF) | (c << 8);
-      break;
-    case 2:
-      x = (x & 0xFF00FFFF) | (c << 16);
-      break;
-    default:
-      x = (x & 0x00FFFFFF) | (c << 24);
-      break;
-    }
-    return x;
+  return x <= y;
 }
-int test_reverseBits(int x) {
-  unsigned int v = x;
-  // swap odd and even bits
-  v = ((v >> 1) & 0x55555555) | ((v & 0x55555555) << 1);
-  // swap consecutive pairs
-  v = ((v >> 2) & 0x33333333) | ((v & 0x33333333) << 2);
-  // swap nibbles ... 
-  v = ((v >> 4) & 0x0F0F0F0F) | ((v & 0x0F0F0F0F) << 4);
-  // swap bytes
-  v = ((v >> 8) & 0x00FF00FF) | ((v & 0x00FF00FF) << 8);
-  // swap 2-byte long pairs
-  v = ( v >> 16 ) | ( v << 16);
-  return (int) v;
-}
-int test_satAdd(int x, int y)
+int test_reverseBytes(int x)
 {
-  if (x > 0 && y > 0 && x+y < 0)
-    return (0x7FFFFFFF);
-  if (x < 0 && y < 0 && x+y >= 0)
-    return (0x80000000);
-  return x + y;
+    unsigned char byte0 = (x >> 0);
+    unsigned char byte1 = (x >> 8);
+    unsigned char byte2 = (x >> 16);
+    unsigned char byte3 = (x >> 24);
+    unsigned result = (byte0<<24)|(byte1<<16)|(byte2<<8)|(byte3<<0);
+    return result;
+}
+int test_bitCount(int x) {
+  int result = 0;
+  int i;
+  for (i = 0; i < 32; i++)
+    result += (x >> i) & 0x1;
+  return result;
+}
+int test_logicalNeg(int x)
+{
+  return !x;
+}
+int test_trueFiveEighths(int x)
+{
+  return (int) (((long long int) x * 5)/8);
 }
 /*
  * Extra credit
  */
-unsigned test_float_abs(unsigned uf) {
-  float f = u2f(uf);
-  unsigned unf = f2u(-f);
-  if (isnan(f))
-    return uf;
-  /* An unfortunate hack to get around a limitation of the BDD Checker */
-  if ((int) uf < 0)
-      return unf;
-  else
-      return uf;
+unsigned test_float_neg(unsigned uf) {
+    float f = u2f(uf);
+    float nf = -f;
+    if (isnan(f))
+ return uf;
+    else
+ return f2u(nf);
 }
-int test_float_f2i(unsigned uf) {
+unsigned test_float_twice(unsigned uf) {
   float f = u2f(uf);
-  int x = (int) f;
-  return x;
-}
-unsigned test_float_half(unsigned uf) {
-  float f = u2f(uf);
-  float hf = 0.5*f;
+  float tf = 2*f;
   if (isnan(f))
     return uf;
   else
-    return f2u(hf);
+    return f2u(tf);
 }
